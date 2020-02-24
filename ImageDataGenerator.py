670d37dc9e46555f6a47d25d5dff7fb0d4a06caa
vikasnataraja <viha4393@colorado.edu>
2020-02-24 15:07:08 -0700

    updated generator to use opencv and separated X and y

--- ImageDataGenerator.py
+++ ImageDataGenerator.py
@@ -8,6 +8,7 @@ from skimage.io import imread
 from skimage.transform import resize
 from keras.utils import Sequence
 import os
+import cv2
 import numpy as np
 
 class ImageGenerator(Sequence):
@@ -15,7 +16,7 @@ class ImageGenerator(Sequence):
     def __init__(self, image_list, label_list, 
                  image_dir, anno_dir, num_classes, 
                  batch_size, resize_shape_tuple, num_channels,
-                 shuffle=True):
+                 to_fit=True, shuffle=True):
         
         self.image_list = image_list
         self.label_list = label_list
@@ -25,36 +26,38 @@ class ImageGenerator(Sequence):
         self.batch_size = batch_size
         self.resize_shape_tuple = resize_shape_tuple
         self.num_channels = num_channels
+        self.to_fit = to_fit
         self.shuffle = shuffle
         self.on_epoch_end()
 
 
-    def __data_generator(self, batch_images):
-        
-        if not os.path.exists(self.image_dir):
-            raise FileNotFoundError('ERROR! The folder {} does not'
-                                    ' exist\n'.format(self.image_dir))
+    def _data_generator_X(self, batch_images):
         
         X = np.zeros([self.batch_size, self.resize_shape_tuple[0], 
                       self.resize_shape_tuple[1], self.num_channels])
+        for i, val in enumerate(batch_images):
+            
+            img = cv2.resize(cv2.imread(os.path.join(self.image_dir, val)), self.resize_shape_tuple)
+
+            X[i,] = img
+            
+            return X
+
+    def _data_generator_y(self, batch_images):
+
         y = np.zeros([self.batch_size, self.resize_shape_tuple[0], 
                       self.resize_shape_tuple[1], self.num_classes])
         
         for i, val in enumerate(batch_images):
-            
-            img = resize(imread(os.path.join(self.image_dir, val)), self.resize_shape_tuple)
 
             # VOC labels are in png format
-            label = resize(imread(os.path.join(self.anno_dir, (os.path.splitext(val)[0]+'.png'))),
-                       self.resize_shape_tuple)
+            label = cv2.resize(cv2.imread(os.path.join(self.anno_dir, val)),self.resize_shape_tuple)
             label = (np.arange(self.num_classes) == label[:,:,None]).astype('float32')
             assert label.shape[2] == self.num_classes,"Error, dimensions do not match"
             
-            X[i,] = img
-            
             y[i] = label
             
-            return X, y
+            return y
                     
     
     def __len__(self):
@@ -67,9 +70,13 @@ class ImageGenerator(Sequence):
         
         batch_images = [self.image_list[k] for k in indices]
         # Generate data
-        X, y = self.__data_generator(batch_images)
+        X = self._data_generator_X(batch_images)
 
-        return X, y
+        if self.to_fit is True:
+            y = self._data_generator_y(batch_images)
+            return X,y
+        else:
+            return y
     
     def on_epoch_end(self):
         self.indices = np.arange(len(self.image_list))
