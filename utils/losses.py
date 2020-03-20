425d847c3b0a01022a4bdad6ced7b1e6929116d6
vikasnataraja <viha4393@colorado.edu>
2020-03-20 13:00:06 -0600

    adding a utils directory for losses and utils

---
+++ utils/losses.py
@@ -0,0 +1,65 @@
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+
+def dice_coefficient_loss(y_true, y_pred):
+  return 1-dice_coefficient(y_true, y_pred)
+
+def dice_coefficient(y_true, y_pred, smooth=1.):
+  y_true_f = K.flatten(y_true)
+  y_pred_f = K.flatten(y_pred)
+  intersection = K.sum(y_true_f * y_pred_f)
+  return (2.*intersection + smooth)/(K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_loss(y_true, y_pred):
+  numerator = 2 * tf.reduce_sum(y_true * y_pred, axis=-1)
+  denominator = tf.reduce_sum(y_true + y_pred, axis=-1)
+
+  return 1 - (numerator + 1) / (denominator + 1)
+
+######################################################
+# Weighted Cross Entropy
+######################################################
+
+def weighted_cross_entropy(y_true, y_pred):
+  return wce_loss(y_true, y_pred)
+
+def convert_to_logits(y_pred):
+  # see https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/python/keras/backend.py#L3525
+  y_pred = tf.clip_by_value(y_pred, K.epsilon(), 1 - K.epsilon())
+
+  return tf.log(y_pred / (1 - y_pred))
+
+def wce_loss(y_true, y_pred):
+  y_pred = convert_to_logits(y_pred)
+  loss = tf.nn.weighted_cross_entropy_with_logits(logits=y_pred, targets=y_true, pos_weight=35.0)
+  # or reduce_sum and/or axis=-1
+  return tf.reduce_mean(loss)
+
+def weighted_categorical_crossentropy(y_true, y_pred):
+  # weights = [0.9,0.05,0.04,0.01]
+  def wcce(y_true, y_pred):
+    Kweights = K.constant(35.0)
+    if not K.is_tensor(y_pred):
+      y_pred = K.constant(y_pred)
+    y_true = K.cast(y_true, y_pred.dtype)
+    return K.categorical_crossentropy(y_true, y_pred) * K.sum(y_true * Kweights, axis=-1)
+  
+  return wcce(y_true, y_pred)
+######################################################
+def jaccard_distance_loss(y_true, y_pred, smooth=100):
+    """Jaccard distance for semantic segmentation.
+    Also known as the intersection-over-union loss.
+    This loss is useful when you have unbalanced numbers of pixels within an image
+    because it gives all classes equal weight.
+    """
+    intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
+    sum_ = K.sum(K.abs(y_true) + K.abs(y_pred), axis=-1)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return (1 - jac) * smooth
+
+def focal_loss(y_true, y_pred, gamma=1.0):
+    y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+    eps = K.epsilon()
+    y_pred = K.clip(y_pred, eps, 1. - eps)
+    return -K.sum(K.pow(1. - y_pred, gamma) * y_true * K.log(y_pred), axis=-1)
---
+++ utils/utils.py
@@ -0,0 +1,89 @@
+import numpy as np
+import tensorflow as tf
+from keras.utils import Sequence
+from keras.utils import to_categorical
+import os
+import cv2
+
+def normalize(img):
+        return (img-img.min())/(img.max()-img.min())
+
+""" 
+Adapted from sources: 
+https://github.com/Vladkryvoruchko/PSPNet-Keras-tensorflow/blob/master/utils/preprocessing.py
+https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+"""
+
+class ImageGenerator(Sequence):
+  """ 
+  Adapted from sources: 
+  https://github.com/Vladkryvoruchko/PSPNet-Keras-tensorflow/blob/master/utils/preprocessing.py
+  https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+  """
+  
+  def __init__(self, image_list, image_dict, label_dict,
+               num_classes, batch_size, input_shape, output_shape,
+               num_channels, to_fit=True, shuffle=False):
+      
+    self.image_list = image_list
+    self.image_dict = image_dict
+    self.label_dict = label_dict
+    self.num_classes = num_classes
+    self.batch_size = batch_size
+    self.input_shape = input_shape
+    self.output_shape = output_shape
+    self.num_channels = num_channels
+    self.to_fit = to_fit
+    self.shuffle = shuffle
+    self.on_epoch_end()
+
+
+  def _data_generator_X(self, batch_images):
+    X = np.zeros((self.batch_size, self.input_shape,
+                    self.input_shape, self.num_channels))
+    
+    for i, val in enumerate(batch_images):
+      in_img = self.image_dict[val]
+      in_img = (in_img-in_img.min())/(in_img.max()-in_img.min())
+      #if self.num_channels<3:
+      in_img = np.reshape(in_img, (in_img.shape[0],in_img.shape[1],self.num_channels))
+      X[i] = in_img
+    return X
+
+  def _data_generator_y(self, batch_images):
+    y = np.zeros((self.batch_size, self.output_shape, 
+                  self.output_shape, self.num_classes))
+    
+    for i, val in enumerate(batch_images):
+      label = self.label_dict[val]
+      #print('label val\n',val)
+      # one-hot encoding of mask labels using Keras. This will transform mask from 
+      # (width x height) to (width x height x num_classes) with 1s and 0s
+      label = np.uint8(to_categorical(label, num_classes=self.num_classes))
+      y[i] = label
+      
+    return y
+              
+  
+  def __len__(self):
+    return int(np.floor(len(self.image_list)/self.batch_size))
+
+  
+  def __getitem__(self, index):
+    indices = self.indices[index*self.batch_size:(index+1)*self.batch_size]
+    
+    batch_images = [self.image_list[k] for k in indices]
+ 
+    # Generate data
+    X = self._data_generator_X(batch_images)
+
+    if self.to_fit:
+      y = self._data_generator_y(batch_images)
+      return X,y
+    else:
+      return X
+  
+  def on_epoch_end(self):
+    self.indices = np.arange(len(self.image_list))
+    if self.shuffle:
+      np.random.shuffle(self.indices)
