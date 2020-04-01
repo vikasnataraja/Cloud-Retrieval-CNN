904c05078bd715ab063e298a1a343fda4799d389
vikasnataraja <viha4393@colorado.edu>
2020-04-01 12:21:51 -0600

    changed top conv layer to 7x7 and removed blank lines

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ def common_skip(prev, num_filters, kernel_size,
   and the convolutional block. Both of those blocks share
   this common functionality.
   """
-  x1 = Conv2D(filters=num_filters, kernel_size=kernel_size, 
+  x1 = Conv2D(num_filters, kernel_size=kernel_size, 
               strides=stride_tuple, dilation_rate=atrous_rate,
               padding=pad_type, use_bias=True)(prev)
   x1 = BatchNorm()(x1)
@@ -38,13 +38,11 @@ def convolution_branch(prev, num_filters, kernel_size,
 
   prev = Conv2D(num_filters, kernel_size=kernel_size, strides=stride_tuple,
                 padding=pad_type, dilation_rate=atrous_rate, use_bias=False)(prev)
-  
   prev = BatchNorm()(prev)
   if name=='halve_feature_map':
       # halve the size of feature map by using same padding, 2x2 pooling
     prev = MaxPooling2D(pool_size=(2,2),padding='same')(prev)  
   return prev
-
 
 def empty_branch(prev):
   return prev
@@ -52,9 +50,7 @@ def empty_branch(prev):
 def convolutional_resnet_block(prev_layer, num_filters, name, kernel_size,
                              stride_tuple, pad_type, atrous_rate=1):
 
-  #prev_layer = BatchNorm()(prev_layer)
   prev_layer = Activation('relu')(prev_layer)
-  
   block_1 = common_skip(prev=prev_layer, num_filters=num_filters, 
                         name=name, kernel_size=kernel_size, 
                         stride_tuple=stride_tuple,
@@ -67,18 +63,14 @@ def convolutional_resnet_block(prev_layer, num_filters, name, kernel_size,
                                pad_type=pad_type,
                                atrous_rate=atrous_rate,
                                name=name)
-  
+ 
   added = Add()([block_1, block_2])
-  
   return added
   
-
 def identity_resnet_block(prev_layer, num_filters, name, kernel_size,
                         stride_tuple, pad_type, atrous_rate=1):
   
-  #prev_layer = BatchNorm()(prev_layer)
   prev_layer = Activation('relu')(prev_layer)
-  
   block_1 = common_skip(prev=prev_layer, num_filters=num_filters, 
                         kernel_size=kernel_size, 
                         stride_tuple=stride_tuple,
@@ -87,18 +79,14 @@ def identity_resnet_block(prev_layer, num_filters, name, kernel_size,
                         name=name)
   
   block_2 = empty_branch(prev_layer)
-
   added = Add()([block_1, block_2])
-  
   return added
-
 
 def ResNet(input_layer):
   
-  x = Conv2D(16, (3, 3), strides=(1, 1), padding='same',
+  x = Conv2D(16, (7, 7), strides=(1, 1), padding='same',
              use_bias=False)(input_layer)
   x = BatchNorm()(x)
-  #print('conv',input_layer.shape)
   x = identity_resnet_block(x, num_filters=16, kernel_size=(3,3),
                             stride_tuple=(1,1), name="identity",
                             pad_type='same', atrous_rate=1)
@@ -141,7 +129,6 @@ def ResNet(input_layer):
                             stride_tuple=(1,1), name="identity",
                             pad_type='same', atrous_rate=4)
   
-  #x = BatchNorm()(x)
   x = Activation('relu')(x)
 
   x = Conv2D(filters=512,kernel_size=(3,3),
@@ -166,7 +153,6 @@ def ResNet(input_layer):
   x = BatchNorm()(x)
   x = Activation('relu')(x)
   
-  print('Finished building ResNet')
   return x
   
 """Spatial Pyramid Pooling"""
@@ -181,7 +167,6 @@ def upsample_bilinear(in_tensor, new_size):
 def spp_block(prev_layer, pool_size_int, feature_map_shape):
 
   #kernel = [(1,1),(2,2),(4,4),(8,8)]
-  #strides = [(1,1),(2,2),(4,4),(8,8)]
   pool_size_tuple = (pool_size_int, pool_size_int)
   pool_layer = AveragePooling2D(pool_size=pool_size_tuple)(prev_layer)
   conv1 = Conv2D(128, (1, 1), strides=(1, 1),
@@ -192,7 +177,6 @@ def spp_block(prev_layer, pool_size_int, feature_map_shape):
   # upsampling
   upsampled_layer = Lambda(upsample_bilinear, 
                            arguments={'new_size':feature_map_shape})(conv1)
-  
   return upsampled_layer
 
 def pyramid_pooling_module(resnet_last, output_shape, pool_sizes=[1,2,4,8]):
@@ -208,14 +192,12 @@ def pyramid_pooling_module(resnet_last, output_shape, pool_sizes=[1,2,4,8]):
 
   # concat all these layers with previous layer. resulted
   # shape=(None,feature_map_size_x,feature_map_size_y,4096)
-  #print('shape of resnet is',resnet_last.shape)
   concat = Concatenate()([resnet_last,
                           pool_block8,
                           pool_block4,
                           pool_block2,
                           pool_block1])
   return concat    
-  
 
 """
 Deconvolution layer comes after concatenation of SPP layers
@@ -225,17 +207,12 @@ From the paper:
 the same size as the input image by the transposed convolution"
 """
 def deconvolution_module(concat_layer, num_classes, output_shape, activation_fn):
-  
   deconv_layer = Conv2DTranspose(filters=num_classes, kernel_size=(16,16),
                                  strides=(1,1),padding='same')(concat_layer)
   
   # output shape needs to be 128,128, so upsample from 16x16
   deconv_layer = Lambda(upsample_bilinear,
                         arguments={'new_size':output_shape})(deconv_layer)
-  
-  #deconv_layer.set_shape((None,128,128,1))
   deconv_layer = BatchNorm()(deconv_layer)
-
   deconv_layer = Activation(activation_fn)(deconv_layer)
-                        
   return deconv_layer
