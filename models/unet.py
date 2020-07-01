b86eaba09a50944961b60561423d5dead241220b
vikasnataraja <viha4393@colorado.edu>
2020-07-01 11:15:06 -0600

    add multi scale experiments

--- models/unet.py
+++ models/unet.py
@@ -76,42 +76,60 @@ def UNet(input_shape, num_channels, num_classes, final_activation_fn):
 def Experimental_UNet(input_shape, num_channels, num_classes, final_activation_fn):
   input_layer = Input((input_shape,input_shape,num_channels))
 
-  conv0 = ConvBlock(layer=input_layer, filters=32, kernel_size=(3,3), strides=(1,1), pad_type='same')
-    
+  avg_pool1 = AveragePooling2D(pool_size=(2,2))(input_layer)
+  avg_pool2 = AveragePooling2D(pool_size=(2,2))(avg_pool1)
+  avg_pool3 = AveragePooling2D(pool_size=(2,2))(avg_pool2)
+  avg_pool4 = AveragePooling2D(pool_size=(2,2))(avg_pool3)
+
   conv1 = ConvBlock(layer=input_layer, filters=64, kernel_size=(3,3), strides=(1,1), pad_type='same')
   pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-  conv2 = ConvBlock(layer=pool1, filters=128, kernel_size=(3,3), strides=(1,1), pad_type='same')
+  scaled_conv1 = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')(avg_pool1)
+  scaled_concat1 = Concatenate(axis=-1)([scaled_conv1, pool1])
+  scaled_concat1 = Dropout(0.2)(scaled_concat1)
+
+  conv2 = ConvBlock(layer=scaled_concat1, filters=128, kernel_size=(3,3), strides=(1,1), pad_type='same')
   pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-  conv3 = ConvBlock(layer=pool2, filters=256, kernel_size=(3,3), strides=(1,1), pad_type='same')
+  scaled_conv2 = Conv2D(filters=128, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')(avg_pool2)
+  scaled_concat2 = Concatenate(axis=-1)([scaled_conv2, pool2])
+  scaled_concat2 = Dropout(0.2)(scaled_concat2)
+
+  conv3 = ConvBlock(layer=scaled_concat2, filters=256, kernel_size=(3,3), strides=(1,1), pad_type='same')
   pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-  conv4 = ConvBlock(layer=pool3, filters=512, kernel_size=(3,3), strides=(1,1), pad_type='same')
+  scaled_conv3 = Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')(avg_pool3)
+  scaled_concat3 = Concatenate(axis=-1)([scaled_conv3, pool3])
+  scaled_concat3 = Dropout(0.2)(scaled_concat3)
+
+  conv4 = ConvBlock(layer=scaled_concat3, filters=512, kernel_size=(3,3), strides=(1,1), pad_type='same')
   pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-  conv5 = ConvBlock(layer=pool4, filters=1024, kernel_size=(3,3), strides=(1,1), pad_type='same')
-  up6 = UpSampleTranspose(layer=conv5, filters=256, kernel_size=(2,2), strides=(1,1), pad_type='same')
-  up6 = Dropout(0.5)(up6)
-  concat6 = Concatenate(axis=-1)([pool3, up6])
+  scaled_conv4 = Conv2D(filters=512, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu')(avg_pool4)
+  scaled_concat4 = Concatenate(axis=-1)([scaled_conv4, pool4])
+  scaled_concat4 = Dropout(0.2)(scaled_concat4)
+
+  conv5 = ConvBlock(layer=scaled_concat4, filters=1024, kernel_size=(3,3), strides=(1,1), pad_type='same')
+  up6 = UpSampleTranspose(layer=conv5, filters=512, kernel_size=(2,2), strides=(1,1), pad_type='same')
+  up6 = Dropout(0.4)(up6)
+  concat6 = Concatenate(axis=-1)([conv4, up6])
 
   conv6 = ConvBlock(layer=concat6, filters=512, kernel_size=(3,3), strides=(1,1), pad_type='same')
-  up7 = UpSampleTranspose(layer=conv6, filters=128, kernel_size=(2,2), strides=(1,1), pad_type='same')
-  up7 = Dropout(0.5)(up7)
-  concat7 = Concatenate(axis=-1)([pool2, up7])
+  up7 = UpSampleTranspose(layer=conv6, filters=256, kernel_size=(2,2), strides=(1,1), pad_type='same')
+  up7 = Dropout(0.4)(up7)
+  concat7 = Concatenate(axis=-1)([conv3, up7])
 
   conv7 = ConvBlock(layer=concat7, filters=256, kernel_size=(3,3), strides=(1,1), pad_type='same')
-  up8 = UpSampleTranspose(layer=conv7, filters=64, kernel_size=(2,2), strides=(1,1), pad_type='same')
-  up8 = Dropout(0.5)(up8)
-  concat8 = Concatenate(axis=-1)([pool1, up8])
+  up8 = UpSampleTranspose(layer=conv7, filters=128, kernel_size=(2,2), strides=(1,1), pad_type='same')
+  up8 = Dropout(0.4)(up8)
+  concat8 = Concatenate(axis=-1)([conv2, up8])
 
   conv8 = ConvBlock(layer=concat8, filters=128, kernel_size=(3,3), strides=(1,1), pad_type='same')
-  up9 = UpSampleTranspose(layer=conv8, filters=32, kernel_size=(2,2), strides=(1,1), pad_type='same')
-  up9 = Dropout(0.5)(up9)
-  concat9 = Concatenate(axis=-1)([conv0, up9])
+  up9 = UpSampleTranspose(layer=conv8, filters=64, kernel_size=(2,2), strides=(1,1), pad_type='same')
+  up9 = Dropout(0.4)(up9)
+  concat9 = Concatenate(axis=-1)([conv1, up9])
 
   conv9 = ConvBlock(layer=concat9, filters=64, kernel_size=(3,3), strides=(1,1), pad_type='same')
-  conv9 = Dropout(0.5)(conv9)
   conv10 = Conv2D(num_classes, kernel_size=(1,1))(conv9)
   conv10 = Activation(final_activation_fn)(conv10)
 
