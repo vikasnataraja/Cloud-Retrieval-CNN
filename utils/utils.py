862aa78c0f005d2053c3ac36bc1524a36c626450
vikasnataraja <viha4393@colorado.edu>
2020-08-10 15:07:41 -0600

    make some minor changes to increase efficiency

--- utils/utils.py
+++ utils/utils.py
@@ -141,13 +141,13 @@ def get_data(data_dir, fnames, rad_keyname, cot_keyname, use_short_idx=False):
 
 def crop_images(img_dict, crop_dims, fname_prefix):
   imgs = np.array(list(img_dict.values()))
-  img_names = np.array(list(img_dict.keys()))
-  imgwidth, imgheight = imgs.shape[1], imgs.shape[2]
+  img_names_length = len(img_dict)
+  width, height = imgs.shape[1:]
   return_imgs = {}
-  counter=0
-  for idx in range(img_names.shape[0]):
-    for hcount, i in enumerate(range(0, imgwidth, int(crop_dims/2))):
-      for vcount, j in enumerate(range(0, imgheight, int(crop_dims/2))):
+  counter = 0
+  for idx in range(img_names_length):
+    for _, i in enumerate(range(0, width, int(crop_dims/2))):
+      for _, j in enumerate(range(0, height, int(crop_dims/2))):
         if len(imgs.shape)>3:
           cropped_img = imgs[idx, i:i+crop_dims, j:j+crop_dims,:]
         else:
