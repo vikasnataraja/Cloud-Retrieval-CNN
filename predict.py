1876112c14b8a81299d166afa2a1693530814603
vikasnataraja <viha4393@colorado.edu>
2020-12-04 10:32:10 -0700

    add functionality for plotting more metrics

--- predict.py
+++ predict.py
@@ -1,15 +1,25 @@
-from keras.models import load_model
-from utils.utils import standard_normalize, resize_img
+from utils.utils import preprocess, get_class_space_data, get_cot_space_data, get_model
 # from utils.model_utils import UpSample
-from utils.losses import focal_loss
-from utils.plot_utils import plot_evaluation, visualize_prediction, plot_stat_metrics, plot_model_comparison
-from sklearn.model_selection import train_test_split
+from utils.plot_utils import plot_model_comparison, plot_all
 from time import perf_counter
 import tensorflow as tf
 import numpy as np
 import os
 import argparse
 import cv2
+import h5py
+
+cot_class_l = np.concatenate((np.arange(0.0, 1.0, 0.1),
+                               np.arange(1.0, 10.0, 1.0),
+                               np.arange(10.0, 20.0, 2.0),
+                               np.arange(20.0, 50.0, 5.0),
+                               np.arange(50.0, 101.0, 10.0)))
+
+# COT classes (use upper boundary)
+cot_class_r = np.append(cot_class_l[1:], 200.0)
+
+# COT classes (use middle)
+cot_class_mid = (cot_class_l+cot_class_r)/2.0
 
 def get_progress(start, total, current_count):
   """
@@ -30,20 +40,6 @@ def get_progress(start, total, current_count):
           print('Progress: {:.0f}%, ~ {:0.1f} min remaining'.format(np.ceil(progress), remaining/60))
       else:  # print in seconds
           print('Progress: {:.0f}%, ~ {}s remaining'.format(np.ceil(progress), remaining))
-
-
-def preprocess(img, resize_dims=None, normalize=False):
-  """ Pre-processing steps for an image """
-  if normalize:
-    img = standard_normalize(img)
-  if resize_dims is not None:
-    img = resize_img(img, resize_dims)
-
-  if len(img.shape) > 2:
-    img = np.reshape(img, (1, img.shape[0], img.shape[1], img.shape[2]))
-  else:
-    img = np.reshape(img, (1, img.shape[0], img.shape[1], 1))
-  return img
 
 
 def predict_on_validation_set(input_data, gt_data, model, image_list=None):
@@ -147,12 +143,40 @@ def predict_cot_on_image(input_img, model, ground_truth_img=None):
   return prediction
 
 
-def main(input_file, file_1d, file_3d, modelpath_1, modelpath_2, compare, figname):
+def predict_on_dataset(input_data, model, use_argmax=False):
+  # ====================================================== #
+  def predict_on_image(input_img, model, use_argmax):
+    temp = model.predict(preprocess(input_img, resize_dims=(model.input_shape[1], model.input_shape[2])))
+    temp = np.reshape(temp.ravel(), model.output_shape[1:])
+    if use_argmax:
+      return np.argmax(temp, axis=-1) # use argmax to get the image
+    return np.dot(temp, cot_class_mid)
+  # ====================================================== #
+  predictions = {}
+  for idx, key in enumerate(list(input_data.keys())):
+    input_img = input_data[key]
+    predictions['data_{}'.format(idx)] = predict_on_image(input_img, model, use_argmax=use_argmax)
+  return predictions
+
+
+def reconstruct(dictionary, num_scenes, dims): 
+  keys = list(dictionary.keys())
+  recon = np.zeros((num_scenes, dims-32, dims-32)) 
+  idx = 0
+  for up_idx in range(num_scenes):
+    for i in range(0, dims-32, 32):
+      for j in range(0, dims-32, 32):
+        recon[up_idx, i:i+32, j:j+32] = dictionary[keys[idx]][:32,:32]
+        idx += 1                    
+  return recon
+
+
+def compare_models(input_file, file_1d, file_3d, modelpath_1, modelpath_2, compare, figname):
   radiances = np.load('{}'.format(input_file), allow_pickle=True).item()
   cot_1d = np.load('{}'.format(file_1d), allow_pickle=True).item()
   cot_3d = np.load('{}'.format(file_3d), allow_pickle=True).item()
   
-  model_1 = load_model('{}'.format(modelpath_1), custom_objects={"tf":tf, "focal_loss":focal_loss})
+  model_1 = get_model(modelpath_1)
   means3d_1, devs3d_1, slopes3d_1 = predict_on_validation_set(radiances, cot_3d, model_1)
 
   means1d, devs1d, slopes1d = get_1d_retrievals(radiances, cot_1d=cot_1d, cot_3d=cot_3d)
@@ -161,7 +185,7 @@ def main(input_file, file_1d, file_3d, modelpath_1, modelpath_2, compare, fignam
     plot_model_comparison(means1d, devs1d, slopes1d, slopes3d_1, label1='1d_ret', label2='3d_ret', figname=figname)
 
   else:
-    model_2 = load_model('{}'.format(modelpath_2), custom_objects={"tf":tf, "focal_loss":focal_loss})
+    model_2 = get_model(modelpath_2)
     means3d_2, devs3d_2, slopes3d_2 = predict_on_validation_set(radiances, cot_3d, model_2)
     plot_model_comparison(means1d, devs1d, slopes3d_1, slopes3d_2, label1='model_1', label2='model_2', figname=figname)
   
@@ -169,10 +193,42 @@ def main(input_file, file_1d, file_3d, modelpath_1, modelpath_2, compare, fignam
   print('\nThe mean slope of 3D retrievals for model_1 is {:0.2f}\n'.format(np.mean(slopes3d_1)))
   print('\nThe mean slope of 3D retrievals for model_2 is {:0.2f}\n'.format(np.mean(slopes3d_2)))
 
+
+def predict_metrics(path_to_model, fdir, input_file, file_1d, file_3d, reconstruct, figname):
+
+  fnames = [file for file in os.listdir(fdir) if file.endswith('.h5')]
+
+  rad_cot_space, cot_true_cot_space, cot_1d_cot_space = get_cot_space_data(fdir, fnames)
+  _, cot_true_class_space, cot_1d_class_space = get_class_space_data(input_file, file_3d, file_1d)
+  model = get_model(path_to_model)
+  prediction_cot_space = predict_on_dataset(rad_cot_space, model, use_argmax=False)
+  prediction_class_space = predict_on_dataset(rad_cot_space, model, use_argmax=True)
+  
+  if reconstruct:
+    recon_true_class_space = reconstruct(cot_true_class_space, num_scenes=5, dims=384)
+    recon_true_cot_space = reconstruct(cot_true_cot_space, num_scenes=5, dims=384)
+
+    recon_pred_1d_class_space = reconstruct(cot_1d_class_space, num_scenes=5, dims=384)
+    recon_pred_1d_cot_space = reconstruct(cot_1d_cot_space, num_scenes=5, dims=384)
+
+    recon_pred_cnn_class_space = reconstruct(prediction_class_space, num_scenes=5, dims=384)
+    recon_pred_cnn_cot_space = reconstruct(prediction_cot_space, num_scenes=5, dims=384)
+
+    recon_input_radiance = reconstruct(rad_class_space, num_scenes=5, dims=384)
+    plot_all(recon_input_radiance, recon_true_cot_space, recon_true_class_space,
+         recon_pred_cnn_cot_space, recon_pred_cnn_class_space,
+         recon_pred_1d_cot_space, recon_pred_1d_class_space,
+         rows=len(recon_input_radiance), random=False, dimensions='480x480', figsize=(42,38))
+  else:
+    plot_all(rad_cot_space, cot_true_cot_space, cot_true_class_space, 
+           prediction_cot_space, prediction_class_space,
+           cot_1d_cot_space, cot_1d_class_space, filename=figname, rows=3, dimensions='64x64', random=True)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file', default=None, type=str,
-                        help="Path to numpy input images file")
+    parser.add_argument('--h5dir', default=None, type=str, help="Path to the directory containing h5 files")
+    parser.add_argument('--input_file', default=None, type=str,help="Path to numpy input images file")
     parser.add_argument('--file_1d', default=None, type=str, help="Path to the 1D retrievals numpy file")
     parser.add_argument('--file_3d', default=None, type=str, help="Path to the 3D retrievals numpy file")
     parser.add_argument('--model_1_path', default=None, type=str, help="Path to the first model")
@@ -180,10 +236,18 @@ if __name__ == '__main__':
     parser.add_argument('--save_figure_as', default='figure.png', type=str, help="Filename for saving figure")
     parser.add_argument('--compare_models', dest='compare', action='store_true',
                       help="Pass --compare_models to compare two models. By default, only one model is used")
+    parser.add_argument('--metrics', dest='metrics', action='store_true',
+                      help="Pass --metrics to plot model evaluation with all metrics")
+    parser.add_argument('--reconstruct', dest='reconstruct', action='store_true',
+                      help="Pass --reconstruct to plot evaluation for reconstructed scene")
     args = parser.parse_args()
+    if args.compare:
+      compare_models(args.input_file, args.file_1d, args.file_3d,
+                     args.model_1_path, args.model_2_path, 
+                     args.compare, figname=args.save_figure_as)
 
-    main(args.input_file, args.file_1d, args.file_3d,
-         args.model_1_path, args.model_2_path, 
-         args.compare, figname=args.save_figure_as)
+    if args.metrics:
+      predict_metrics(args.model_1_path, args.h5dir, args.input_file,
+                      args.file_1d, args.file_3d, args.reconstruct, args.save_figure_as)
 
 
