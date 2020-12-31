4806ad5888c34a6f7f6f905e0fada5126fa1a689
vikasnataraja <viha4393@colorado.edu>
2020-12-31 09:58:29 -0700

    fix typo and indentation

--- utils/visualize.py
+++ utils/visualize.py
@@ -5,23 +5,23 @@ import os
 
 def visualize_layer(model, img, layer_idx, output_dir=os.getcwd(), fname=None, colormap=None, colorbar=False):
   """
-  Visalizes feature maps of a layer of a saved Keras model and saved figure.
+  Visualizes feature maps of a layer of a saved Keras model and saved figure.
 
   Args:
-	- model: Keras model.
-	- img: arr, image for which feature maps needs to be visualized using the model.
-	       Must be pre-processed and either 2D or 3D. Will be turned into a 4D tensor 
-	       within this function.
-	- layer_idx: int, index of the layer for which the feature maps need to be visualized.
-	- output_dir: str, directory where the figure will be saved. If it doesn't exist,
-		      it will be created
-	- fname: str, filename of the saved figure. If None, will be saved under layer_idx name, 
-		 e.g: layer_50.png
-	- colormap: str, the colormap that has to be used during visualization. Has to be
-		    one among matplotlib.pyplot's supported colormaps. If None, will default to
-		    matploltlib's default 'viridis'.
-	- colorbar: bool, set to True if colorbars need to be added beside each feature map.
-		    Disabled by default.
+    - model: Keras model.
+    - img: arr, image for which feature maps needs to be visualized using the model.
+           Must be pre-processed and either 2D or 3D. Will be turned into a 4D tensor 
+           within this function.
+    - layer_idx: int, index of the layer for which the feature maps need to be visualized.
+    - output_dir: str, directory where the figure will be saved. If it doesn't exist,
+    	         it will be created
+    - fname: str, filename of the saved figure. If None, will be saved under layer_idx name, 
+             e.g: layer_50.png
+    - colormap: str, the colormap that has to be used during visualization. Has to be
+                one among matplotlib.pyplot's supported colormaps. If None, will default to
+	        matploltlib's default 'viridis'.
+    - colorbar: bool, set to True if colorbars need to be added beside each feature map.
+                Disabled by default.
   """
 
   if len(img.shape)>2:
@@ -35,9 +35,9 @@ def visualize_layer(model, img, layer_idx, output_dir=os.getcwd(), fname=None, c
   # set columns to 6 to improve view
   cols = 6
   if activation_map.shape[-1] >= 64:
-      rows = 6
+    rows = 6
   else:
-      rows = int(activation_map.shape[-1]/cols)
+    rows = int(activation_map.shape[-1]/cols)
   idx = 1
   fig = plt.figure(figsize=(16,12))
    
@@ -71,3 +71,5 @@ def visualize_layer(model, img, layer_idx, output_dir=os.getcwd(), fname=None, c
   fig.savefig('{}.png'.format(os.path.join(output_dir,fname)), dpi=100)
   print('Saved figure as {}.png'.format(os.path.join(output_dir,fname)))
   plt.close();
+
+  
